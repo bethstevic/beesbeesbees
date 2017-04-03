e247660adeb79e4ab614f57317693a5ef6aa206e
YOUR NAME <bethstevic89@gmail.com>
2017-04-03 13:25:20 -0700

    Set up HoneyMakerBee.js file

--- src/Bee.js
+++ src/Bee.js
@@ -1,10 +1,11 @@
 var Bee = function() {
-  debugger;
   this.age = 5;
   this.color = 'yellow';
   this.job = 'keep on growing';
 };
 
 Bee.prototype = new Grub;
+
+Bee.prototype.constructor = Bee;
 
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
 var HoneyMakerBee = function() {
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+
 };
 
+HoneyMakerBee.prototype = new Bee;
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  return this.honeyPot += 1;
+};
+
+HoneyMakerBee.prototype.giveHoney = function () {
+  return this.honeyPot -= 1;
+};
